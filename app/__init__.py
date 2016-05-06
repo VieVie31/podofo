e4072bd4096eef92e7124315b8c71198ecf38640
VieVie31 <orissermaroix@gmail.com>
2016-05-06 17:39:25 +0000

    Modifying the upload
    Adding the pdf/ directory to the .gitignore for not commiting the pdf
    Making differents porcedures for GET and POST on upload functions in the view
    Modifing search.css for the upload form

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, abort, session
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['PDF_DIR'] = './pdf/'
 
 from app import views
 
--- app/views.py
+++ app/views.py
@@ -1,15 +1,36 @@
 from flask import Flask, render_template, request, redirect, url_for, abort, session
+from werkzeug import secure_filename
 from app import app
- 
+from time import time
+from os import listdir
+
 @app.route('/')
 @app.route('/index')
 def index_page():
     return "YO ! :D"
 
-@app.route('/search')
+@app.route('/search', methods=['GET'])
 def search_page():
-    return render_template('search.html')
+    query = request.args.get('s')
+    if not query:
+        return render_template('search.html')
+    return "Here will be the search results for {}".format(query)
 
-@app.route('/upload')
+@app.route('/upload', methods=['GET'])
 def upload_page():
-    return "Here will be the upload form..."
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def uploaded_page():
+    try:
+	if len(listdir(app.config['PDF_DIR'])) > 50:
+	    return "Too many pdf all ready uploaded..."
+	uploaded_file = request.files['file']
+	file_name = uploaded_file.filename
+	if file_name: #TODO : add more contidions (size, type, allready upld, etc)
+            file_name = str(int(time())) + "_" + secure_filename(file_name)
+	    uploaded_file.save(app.config['PDF_DIR'] + file_name)
+	    return "File {} successfully uploaded... <a href='/search'>search</a>.".format(uploaded_file.filename)
+    except:
+	return "Fail to upload"
+
