49e2ca9794dab63361a44baa59eac1557cfd997b
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2021-02-11 20:14:46 +0100

    made sure app can run without privileges and using proper shebang

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-#!flask/bin/python
+#!/usr/bin/env python
 from app import app
 
-app.run(host="0.0.0.0", port=80, debug=True, threaded=True)
+app.run(host="0.0.0.0", port=8000, debug=True, threaded=True)
 
